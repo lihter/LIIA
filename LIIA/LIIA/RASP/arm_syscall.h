90b99c1325ee6258ae0d3b32251d1246d21370bd
markolihter <lihter.marko@gmail.com>
2023-05-19 11:34:09 +0200

    Removing security features

--- LIIA/LIIA/RASP/arm_syscall.h
+++
@@ -1,7 +0,0 @@
-#ifndef arm_syscall_h
-
-#define arm_syscall_h
-#include <stdio.h>
-void assembly_deny_debugger_attach(void) __attribute__((always_inline));
-
-#endif /* arm_syscall_h */
--- LIIA/LIIA/main.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-import UIKit
-
-// Anti debugging check
-assembly_deny_debugger_attach ()
-
-UIApplicationMain(CommandLine.argc, CommandLine.unsafeArgv, nil, NSStringFromClass(AppDelegate.self))
